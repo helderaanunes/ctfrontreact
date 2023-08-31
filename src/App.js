ee432c49e0e0f7ec58d97c3edd6737403c31a72d
Helder <helder.nunes@afogados.ifpe.edu.br>
2023-08-31 09:00:50 -0300

    iniciando consumir api rest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import CadastrarDoenca from './CadastrarDoenca';
 import ListarDoenca from './ListarDoenca';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  
+  const [ doencas, setDoencas] = useState([]);
+  useEffect(()=>{
+  fetch ("http://localhost:8080/doencas")
+  .then(retorno => retorno.json())
+  .then(retornoConvertidoEmJson => setDoencas(retornoConvertidoEmJson))
+  },[]) //esse colchete é para realizar a requisição uma vez
+
   return (
     <div className="App">
+      <p>{JSON.stringify(doencas)}</p>
       <CadastrarDoenca></CadastrarDoenca>
       <ListarDoenca></ListarDoenca>
     </div>
