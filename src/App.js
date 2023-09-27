77de69dada8153f7c93f7f4ef02181d21281af50
Helder <helder.nunes@afogados.ifpe.edu.br>
2023-09-27 11:38:23 -0300

    limpando formulário

--- src/App.js
+++ src/App.js
@@ -17,10 +17,43 @@ function App() {
       .then((retornoConvertidoEmJson) => setDoencas(retornoConvertidoEmJson));
   }, []); //esse colchete é para realizar a requisição uma vez
 
+  const eventoDigitar = (e) => {
+    setObjDoenca({ ...objDoenca, [e.target.name]: e.target.value });
+  };
+
+  const limparFormulario = () => {
+    setObjDoenca(doenca);
+  }
+
+  const cadastrar = () => {
+    fetch("http://localhost:8080/doencas", {
+      method: "post",
+      body: JSON.stringify(objDoenca),
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((retorno) => retorno.json())
+      .then((retornoConvertidoEmJson) => {
+        if (retornoConvertidoEmJson.mensagem !== undefined) {
+          alert(retornoConvertidoEmJson.mensagem);
+        } else {
+          setDoencas([...doencas, retornoConvertidoEmJson]);
+          alert("Salvo com sucesso!");
+          limparFormulario();
+        }
+      });
+  };
+
   return (
     <div className="App">
-      <p>O que recebeu foi: {JSON.stringify(doenca)}</p>
-      <CadastrarDoenca></CadastrarDoenca>
+      <p>O que recebeu foi: {JSON.stringify(objDoenca)}</p>
+      <CadastrarDoenca
+        eventoTeclado={eventoDigitar}
+        eventoCadastrar={cadastrar}
+        obj={objDoenca}
+      ></CadastrarDoenca>
       <ListarDoenca lista={doencas}></ListarDoenca>
     </div>
   );
--- src/CadastrarDoenca.js
+++ src/CadastrarDoenca.js
@@ -1,10 +1,12 @@
-function CadastrarDoenca(){
+function CadastrarDoenca({eventoTeclado , eventoCadastrar,obj}){
 
     return(
     
     <form>
-        <input type='text' placeholder="Descrição" className="form-control" />
-        <input type="button" value="Salvar" className="btn btn-outline-success"/>
+        <input type='text' value={obj.descricao} onChange={eventoTeclado} name='descricao' placeholder="Descrição" className="form-control" />
+       
+       
+        <input type="button" value="Salvar" onClick={eventoCadastrar} className="btn btn-outline-success"/>
     </form>
     
     )
